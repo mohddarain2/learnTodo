2571740037057cb2c2c51895147c4c2bdd079903
mohddarain2 <mohddarain2@gmail.com>
2023-07-18 21:45:54 +0530

    slow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Header from './Component/Header'
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [storeData, setStoredata] = useState([]);
 
-function App() {
+  const fun = () => {
+    setStoredata([...storeData, { email, name }])
+    setName("")
+    setEmail("")
+  }
+  const deletefun=(index)=>{
+    storeData.splice(index,1);
+    setStoredata([...storeData]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className=''>
+      <Header />
+      <div className='inputs'>
+        <Stack direction="row" spacing={2}>
+          <TextField id="filled-basic" label="Name" variant="filled" value={name} onChange={(e) => { setName(e.target.value) }} />
+          <TextField id="filled-basic" label="Email" variant="filled" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+          <Button variant="contained" color="success" onClick={fun}>
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+      <div className='container' >
+        <div className='data-container'>
+          <h3>Name</h3>
+          <h3>Email</h3>
+          <h3>Remove</h3>
+        </div>
+        {
+          storeData && storeData.map((element, index) => (
+            <div className='mapData' key={index+element}>
+              <div>{element.name}</div>
+              <div>{element.email}</div>
+              <Button variant="outlined" color="error" onClick={()=>deletefun(index)}>
+             <DeleteIcon/>
+              </Button>
+            </div>
+          ))
+        }
+      </div>
+    </div >
+  )
 }
 
-export default App;
+export default App
+
